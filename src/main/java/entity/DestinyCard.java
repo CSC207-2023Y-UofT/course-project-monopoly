09be6836bb41307775541827054c5bffa75d78be
Hecate <hecatelixyue@gmail.com>
2023-07-13 19:09:07 -0400

    Implement the destiny card pool generator

--- src/main/java/entity/DestinyCard.java
+++ src/main/java/entity/DestinyCard.java
@@ -8,10 +8,26 @@ import java.util.ArrayList;
 public class DestinyCard {
 
     private String message;
+
+    /**
+     * There are three elements in the arraylist:
+     * 1st (Integer): represents the value of change of the money
+     * 2nd (Integer): represents the value of change of the position or the id of the target block
+     * 3rd (String): represents the statuses
+     */
     private ArrayList<Object> actions;
 
     public DestinyCard(String message, ArrayList<Object> actions) {
         this.message = message;
         this.actions = actions;
+    }
+
+    @Override
+    public String toString() {
+        return "DestinyCard{" +
+                "message='" + message + '\'' +
+                ", actions= " + "money: " + actions.get(0) + " position: "
+                + actions.get(1) + " statuses: " + actions.get(2) +
+                '}';
     }
 }
