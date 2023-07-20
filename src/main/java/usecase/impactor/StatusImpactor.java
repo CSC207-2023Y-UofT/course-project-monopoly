c8f6d1e831ab8e74daf6dfb6985754f7c202f7ae
QingYang <1047867934@qq.com>
2023-07-20 19:02:20 -0400

    first commit for StatusImpactor and StatusChecker

--- src/main/java/usecase/impactor/StatusImpactor.java
+++ src/main/java/usecase/impactor/StatusImpactor.java
@@ -3,17 +3,43 @@ package usecase.impactor;
 import entity.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * It is an impactor impacting the status attribute of the player
  */
 public class StatusImpactor {
 
-    public void addStatus(Player player, String status){
-        player.addStatus(status);
-    }
+    public void innitStatus(Player player){
+        if(player.getStatus() == null) {
+            HashMap<String, Integer> initialStatus = new HashMap<>();
+            initialStatus.put("movable", 1);
+            initialStatus.put("playable", 1);
+            player.setStatus(initialStatus);
+        }
+        }
+    public void changeStatus(Player player, String name, Integer status)
+    {
+        /*
+        use this method to change the Status of the player due to different event
+         * */
 
-    public void removeStatus(Player player, String status){
-        player.removeStatus(status);
+        HashMap<String, Integer> original = player.getStatus();
+        original.replace(name,status);
+        player.setStatus(original);
+
+    }
+    public void changeStatus(Player player)
+    {
+        // use this method to change the Status of the player automatically
+        // (for counting the rounds in exam center)
+        HashMap<String, Integer> original = player.getStatus();
+        if(original.get("movable") <=0)
+        {
+            int value = original.get("movable");
+            value += 1;
+            original.replace("movable", value);
+        }
     }
 }
