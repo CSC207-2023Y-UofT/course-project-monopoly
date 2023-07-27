b4b2043e416ea7889409113b115680f58e3dbda3
Hecate <hecatelixyue@gmail.com>
2023-07-27 19:46:38 -0400

    Implement the StartingPointUseCase

--- src/main/java/usecase/StartingPointUseCase.java
+++ src/main/java/usecase/StartingPointUseCase.java
@@ -1,8 +1,15 @@
 package usecase;
 
+import entity.Player;
+import entity.StartingPoint;
+import usecase.impactor.MoneyImpactor;
+
 /**
  * Represent the use case when the player passing through the starting point
  */
 public class StartingPointUseCase {
-
+    public static void giveBonus(Player player, StartingPoint startingPoint){
+        int bonus = startingPoint.getBonus();
+        MoneyImpactor.deduct(bonus * -1, player);
+    }
 }
