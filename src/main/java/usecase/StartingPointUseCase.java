8f9ee08a2af21a26b71c0143138e70c3521cbe2f
Hecate <hecatelixyue@gmail.com>
2023-07-27 19:41:55 -0400

    Implement the property generator

--- src/main/java/usecase/StartingPointUseCase.java
+++ src/main/java/usecase/StartingPointUseCase.java
@@ -1,8 +1,5 @@
 package usecase;
 
-import entity.Player;
-import entity.StartingPoint;
-import usecase.impactor.MoneyImpactor;
 
 /**
  * Represent the use case when the player passing through the starting point
