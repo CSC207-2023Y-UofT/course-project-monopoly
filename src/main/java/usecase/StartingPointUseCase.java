4d87de27bb89df607ef49db65fbc67253d4ca727
Hecate <hecatelixyue@gmail.com>
2023-07-27 19:41:00 -0400

    Implement the property generator

--- src/main/java/usecase/StartingPointUseCase.java
+++ src/main/java/usecase/StartingPointUseCase.java
@@ -8,8 +8,5 @@ import usecase.impactor.MoneyImpactor;
  * Represent the use case when the player passing through the starting point
  */
 public class StartingPointUseCase {
-    public static void giveBonus(Player player, StartingPoint startingPoint){
-        int bonus = startingPoint.getBonus();
-        MoneyImpactor.deduct(bonus * -1, player);
-    }
+
 }
