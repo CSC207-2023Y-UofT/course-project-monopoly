fb03c097aba5bb901788d75324a3ac0887a33de8
Hecate <hecatelixyue@gmail.com>
2023-07-29 15:18:29 -0400

    Add javadoc and exception handling for DestinyCardChooser

--- src/main/java/useCases/DestinyCardChooser.java
+++ src/main/java/useCases/DestinyCardChooser.java
@@ -5,12 +5,27 @@ import entity.DestinyCard;
 
 import java.util.ArrayList;
 
-
+/**
+ * A class for choosing destiny cards from the destiny block.
+ */
 public class DestinyCardChooser {
+
+    /**
+     * Randomly selects a destiny card from the given destiny block.
+     *
+     * @param destiny The Destiny block containing the pool of destiny cards.
+     * @return A randomly chosen DestinyCard from the pool, or null if the card pool is empty.
+     */
     public static DestinyCard chooseCard(Destiny destiny){
         ArrayList<DestinyCard> cardPool = destiny.getDestinyCardPool();
-        int index = (int)(Math.random() * cardPool.size());
 
-        return cardPool.get(index);
+        try {
+            int index = (int) (Math.random() * cardPool.size());
+            return cardPool.get(index);
+        } catch (IndexOutOfBoundsException e){
+            // Handle the case where the card pool is empty or has an invalid index.
+            // For simplicity, we return null to indicate no valid card was chosen.
+            return null;
+        }
     }
 }
