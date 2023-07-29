6f1738001f20214f062a382e2ae508d640b53e7a
Hecate <hecatelixyue@gmail.com>
2023-07-29 15:52:19 -0400

    Modify the path of the data files for more convenient searching

--- src/main/java/useCases/DestinyCardExecutor.java
+++ src/main/java/useCases/DestinyCardExecutor.java
@@ -10,24 +10,57 @@ import useCases.impactor.StatusImpactor;
 import java.util.ArrayList;
 
 /**
- * It is an executor executing the actions of a destiny card
+ * The DestinyCardExecutor is responsible for executing the actions of a destiny card.
+ * It applies the effects of the destiny card on the game data and the player.
  */
 public class DestinyCardExecutor {
+
+    /**
+     * Executes the actions of a given destiny card on the game data and the player.
+     *
+     * @param data   The game data containing information about the game state.
+     * @param player The player on whom the destiny card's actions will be applied.
+     * @param card   The destiny card whose actions will be executed.
+     * @return The message associated with the destiny card after execution.
+     */
     public static String executeCard(GameData data, Player player, DestinyCard card){
-        ArrayList<Object> actions = card.getActions();
+        try {
+            if (data == null || player == null || card == null) {
+                throw new NullPointerException("Data, player, and card must not be null.");
+            }
+            ArrayList<Object> actions = card.getActions();
 
-        MoneyImpactor.deduct(-((int)actions.get(0)), player);
+            if (actions.size() != 3) {
+                throw new IllegalArgumentException("Invalid action data in the destiny card.");
+            }
 
-        int position = (int)actions.get(1);
+            int moneyChange = -((int) actions.get(0));
+            if (moneyChange != 0) {
+                MoneyImpactor.deduct(moneyChange, player);
+            }
 
-        if (position > 100){
-            PositionImpactor.absoluteMove(data, position);
-        } else {
-            PositionImpactor.relativeMove(data, position);
+            int position = (int) actions.get(1);
+
+            if (position >= 100) {
+                PositionImpactor.absoluteMove(data, position);
+            } else if (position != 0){
+                PositionImpactor.relativeMove(data, position);
+            }
+
+            int statusChange = (int) actions.get(2);
+            if (statusChange != 0) {
+                StatusImpactor.changeStatus(player, "movable", statusChange);
+            }
+
+            return card.getMessage();
+        } catch (NullPointerException e) {
+            // Handle NullPointerException (data, player, or card is null)
+            System.err.println("Data, player, and card must not be null.");
+            return "Choose card again";
+        } catch (IllegalArgumentException e) {
+            // Handle IllegalArgumentException (invalid action data in destiny card)
+            System.err.println("Invalid action data in the destiny card.");
+            return "Choose card again";
         }
-
-        StatusImpactor.changeStatus(player, "movable", (int)actions.get(2));
-
-        return card.getMessage();
     }
 }
