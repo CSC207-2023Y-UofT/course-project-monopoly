0dff1ff64f0997db25290ce69a50bc32e3815a01
Hecate <hecatelixyue@gmail.com>
2023-07-30 01:01:03 -0400

    Modify the test data. Handle the situation when the card makes the player move and pass through the starting point. Write tests for DestinyCardExecutor

--- src/test/java/useCases/DestinyCardExecutorTest.java
+++ src/test/java/useCases/DestinyCardExecutorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import useCases.generator.DestinyCardPoolGenerator;
 import useCases.impactor.PositionImpactor;
+import useCases.impactor.StatusImpactor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for {@link DestinyCardExecutor}.
+ */
 class DestinyCardExecutorTest {
     static GameData data;
 
@@ -23,8 +27,13 @@ class DestinyCardExecutorTest {
 
     static ArrayList<DestinyCard> cardPool;
 
+    /**
+     * Sets up the test environment before all test cases are executed.
+     * Initializes the game data and player objects with test data.
+     */
     @BeforeAll
     static void setup(){
+        // Prepare test game data
         ArrayList<Block> blocks = new ArrayList<>();
 
         StartingPoint startingPoint = new StartingPoint(100, 100);
@@ -38,9 +47,8 @@ class DestinyCardExecutorTest {
         ExamCenter examCenter = new ExamCenter(102);
         blocks.add(examCenter);
 
-
-
         player = new Player(0, 1000);
+        StatusImpactor.initStatus(player);
         ArrayList<Player> players = new ArrayList<>(List.of(player));
 
         HashMap<Integer, ArrayList<Player>> position = new HashMap<>();
@@ -55,6 +63,12 @@ class DestinyCardExecutorTest {
         data = new GameData(1, blocks, players, position);
     }
 
+    // Test methods
+
+    /**
+     * Test the execution of a destiny card with null game data.
+     * The method should handle the null data and return an appropriate message.
+     */
     @Test
     void testExecuteCardWithNullData(){
         DestinyCard chosenCard = DestinyCardChooser.chooseCard(destiny);
@@ -66,6 +80,10 @@ class DestinyCardExecutorTest {
         assertEquals("Choose card again", message);
     }
 
+    /**
+     * Test the execution of a destiny card with missing action data.
+     * The method should handle the invalid action data and return an appropriate message.
+     */
     @Test
     void testExecuteCardWithMissingActionData(){
         DestinyCard card = new DestinyCard("It's time for the FINAL!", new ArrayList<>(Arrays.asList(0,0)));
@@ -75,6 +93,10 @@ class DestinyCardExecutorTest {
         assertEquals("Choose card again", message);
     }
 
+    /**
+     * Test the execution of a destiny card with invalid action data.
+     * The method should handle the invalid action data and return an appropriate message.
+     */
     @Test
     void testExecuteCardWithInvalidActionData(){
         DestinyCard card = new DestinyCard("It's time for the FINAL!", new ArrayList<>(Arrays.asList(0,0, "N/A")));
@@ -84,6 +106,10 @@ class DestinyCardExecutorTest {
         assertEquals("Choose card again", message);
     }
 
+    /**
+     * Test the execution of a destiny card that deducts money from the player.
+     * The player's money should be reduced by the specified amount in the destiny card.
+     */
     @Test
     void testExecuteCardDeductMoney(){
         DestinyCard card = cardPool.get(0);
@@ -93,6 +119,10 @@ class DestinyCardExecutorTest {
         assertEquals(700, player.getMoney());
     }
 
+    /**
+     * Test the execution of a destiny card that give money to the player.
+     * The player's money should be increased by the specified amount in the destiny card.
+     */
     @Test
     void testExecuteCardGiveMoney(){
         DestinyCard card = cardPool.get(1);
@@ -102,6 +132,9 @@ class DestinyCardExecutorTest {
         assertEquals(1600, player.getMoney());
     }
 
+    /**
+     * Test the execution of a destiny card that allows the player advance a specific distance.
+     */
     @Test
     void testExecuteCardAdvance(){
         DestinyCard card = cardPool.get(2);
@@ -111,6 +144,22 @@ class DestinyCardExecutorTest {
         assertEquals(101, player.getPosition());
     }
 
+    /**
+     * Test the execution of a destiny card that allows the player to pass the starting point.
+     * The player should be given the bonus when passing through the starting point
+     */
+    @Test
+    void testExecuteCardPassStartingPoint(){
+        DestinyCard card = cardPool.get(2);
+
+        DestinyCardExecutor.executeCard(data,player,card);
+
+        assertEquals(1100, player.getMoney());
+    }
+
+    /**
+     * Test the execution of a destiny card that allows the player move back a specific distance.
+     */
     @Test
     void testExecuteCardMoveBack(){
         DestinyCard card = cardPool.get(3);
@@ -120,6 +169,9 @@ class DestinyCardExecutorTest {
         assertEquals(101, player.getPosition());
     }
 
+    /**
+     * Test the execution of a destiny card that teleports the player to a specific block.
+     */
     @Test
     void testExecuteCardTeleportation(){
         DestinyCard card = cardPool.get(4);
@@ -129,6 +181,9 @@ class DestinyCardExecutorTest {
         assertEquals(102, player.getPosition());
     }
 
+    /**
+     * Test the execution of a destiny card that causes the player to miss a certain number of rounds.
+     */
     @Test
     void testExecuteCardMissRounds(){
         DestinyCard card = cardPool.get(5);
@@ -138,6 +193,10 @@ class DestinyCardExecutorTest {
         assertEquals(-1, player.getStatus().get("movable"));
     }
 
+    /**
+     * Cleans up the test environment after each test case is executed.
+     * Resets the player's money and position to initial values.
+     */
     @AfterEach
     void teardown(){
         player.setMoney(1000);
