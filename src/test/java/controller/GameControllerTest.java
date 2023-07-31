09629226a3cf69e7c31bd6ab6a895a233280ab73
NeverSoGoodStart <yuyangzhao2001@gmail.com>
2023-07-31 15:39:35 +0800

    Removed MainTest, added Javadocs for the other tests

--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -12,10 +12,18 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
 
+/**
+ * Test class for {@link GameController}.
+ * This class contains test cases to verify the behavior of the {@link GameController} class methods.
+ */
 public class GameControllerTest {
     static GameController gc;
     static GameData gd;
 
+    /**
+     * Set up a common files for test purposes
+     * Set up an instance for {@link GameData} and {@link GameController}
+     */
     @BeforeEach
     void setup(){
         String propertiesFile = "data/test/properties_test.csv";
@@ -39,17 +47,28 @@ public class GameControllerTest {
         gc = new GameController(gd);
     }
 
+    /**
+     * Testing for {@link GameController#isGameOver()} method
+     */
     @Test
     public void testIsGameOver() {
         assertFalse(gc.isGameOver());
     }
 
+    /**
+     * Testing for {@link GameController#getMaxMoneyPlayer()} method
+     * It should be the current player
+     */
     @Test
     public void testGetMaxMoneyPlayer() {
-        // It should choose the last one
-        assertEquals(gc.getMaxMoneyPlayer().getUserId(), 3);
+        gd.currentPlayer.setMoney(1000000);
+        assertEquals(gc.getMaxMoneyPlayer(), gd.currentPlayer);
     }
 
+    /**
+     * Testing for {@link GameController#updatePlayablePlayer()} method
+     * I removed one player, so the player count should decrease by 1
+     */
     @Test
     public void testUpdatePlayablePlayer() {
         // It should remove one player count
@@ -62,22 +81,38 @@ public class GameControllerTest {
         assertEquals(gd.playerNum, 3);
     }
 
+    /**
+     * Testing for {@link GameController#isCurrentMovable()} method
+     * The current player should be able to move
+     */
     @Test
     public void testIsCurrentMovable() {
         assertTrue(gc.isCurrentMovable());
     }
 
+    /**
+     * Testing for {@link GameController#settleOneRound()} method
+     * Checking that round number increased by 1
+     */
     @Test
     public void testSettleOneRound() {
         gc.settleOneRound();
         assertEquals(gd.gameRounds, 1);
     }
 
+    /**
+     * Testing for {@link GameController#randomDice()} method
+     * Checking that the dice falls within range of 1 to 6 (inclusive)
+     */
     @Test
     public void testRandomDice() {
         assertTrue(1 <= gc.randomDice() && gc.randomDice() <= 6);
     }
 
+    /**
+     * Testing for {@link GameController#finish()} method
+     * Checking for the output, where the winner is everyone
+     */
     @Test
     public void testFinish() {
         // Setup output stream for testing
