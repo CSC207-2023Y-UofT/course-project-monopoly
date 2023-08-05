07a5881986689b2c2fd1303ae0decc93818bfe69
Hecate <hecatelixyue@gmail.com>
2023-08-04 22:54:04 -0400

    Create data gateway for generators

---
+++ src/main/java/datagateways/DestinyCardDataManager.java
@@ -0,0 +1,44 @@
+package datagateways;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * The DestinyCardDataManager class is responsible for reading destiny card data from a CSV file and providing it as an ArrayList of String arrays.
+ */
+public class DestinyCardDataManager implements DataManager{
+
+    /**
+     * Creates destiny cards based on the information in the specified CSV file.
+     *
+     * @param fileName The name of the CSV file containing destiny card information.
+     * @return An ArrayList of String arrays representing the data read from the CSV file.
+     */
+    public ArrayList<String[]> readData(String fileName){
+
+        ArrayList<String[]> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = br.readLine()) != null){
+                String[] values = line.split(DELIMITER);
+                if (values.length < 4) {
+                    // Handle the case where there are missing values in the CSV line
+                    throw new IllegalArgumentException("Invalid data in the CSV file: " + line);
+                }
+                lines.add(values);
+            }
+        } catch (
+                FileNotFoundException e) {
+            System.err.println("The specified file '" + fileName + "' does not exist or cannot be found.");
+        } catch (IOException e) {
+            System.err.println("Error occurred while reading the file: " + e.getMessage());
+        }
+
+        return lines;
+    }
+}
