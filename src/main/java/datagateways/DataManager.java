c178b328fc67e8f10c6b3704db946eb44c384dc5
HLeiTR <sy.hung@mail.utoronto.ca>
2023-08-07 14:10:34 +0800

    Updated DataManager

--- src/main/java/datagateways/DataManager.java
+++ src/main/java/datagateways/DataManager.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 public interface DataManager <T>{
 
     /**
-     * Reads data from an input with the specified filename and returns it as an ArrayList of String arrays.
-     * Each String array represents a row of data in the CSV file, with each element being a value from a column.
+     * Reads data from an input with a specific form and returns it as an ArrayList of String arrays.
+     * Each String array represents a row of data, with each element being a value from a column.
      *
-     * @param input The input to the method
+     * @param input The input to the method, can be filename for CSV, database, etc.
      * @return An ArrayList of String arrays representing the data read from the CSV file.
      */
     ArrayList<String[]> readData(T input);
