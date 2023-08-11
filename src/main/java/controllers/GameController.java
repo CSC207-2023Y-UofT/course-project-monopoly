45b5a3ac0add3d011595d215de81f2147cda6eaf
Noahkong12 <noahkong88@gmail.com>
2023-08-10 23:13:20 -0400

    Changes by alwyn

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -175,12 +175,13 @@ public class GameController {
      */
     public Player finish() {
         if (data.currentPlayers.size() == 0) {
+            System.out.println("All player has broken up, there is no winner!");
             return null;
-//            System.out.println("All player has broken up, there is no winner!");
+
         }
 
         if (data.currentPlayers.size() == 1) {
-//            System.out.println("Game over! Winner is Player " + data.currentPlayers.get(0).getUserId());
+            System.out.println("Game over! Winner is Player " + data.currentPlayers.get(0).getUserId());
             return data.currentPlayers.get(0);
         }
         
@@ -191,14 +192,15 @@ public class GameController {
                 winner = data.currentPlayers.get(i);
             }
         }
+
+        StringBuilder message = new StringBuilder("Game over! winner is ");
+        for (Player player: data.currentPlayers) {
+            if (player.getMoney() == winner.getMoney())
+                message.append("Player ").append(player.getUserId()).append(" and ");
+        }
+        message.delete(message.length()-5, message.length());
+        message.append("!");
+        System.out.println(message);
         return winner;
-//        StringBuilder message = new StringBuilder("Game over! winner is ");
-//        for (Player player: data.currentPlayers) {
-//            if (player.getMoney() == winner.getMoney())
-//                message.append("Player ").append(player.getUserId()).append(" and ");
-//        }
-//        message.delete(message.length()-5, message.length());
-//        message.append("!");
-//        System.out.println(message);
     }
 }
